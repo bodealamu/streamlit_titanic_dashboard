fafe0566c8c460e20a9bd949920b6872e008f28d
olabodealamu <bodealamu007@gmail.com>
2025-03-07 08:01:16 -0600

    changes made to layout

--- app.py
+++ app.py
@@ -5,4 +5,18 @@ st.set_page_config(layout='wide')
 st.title('Titanic Dashboard')
 
 df = pd.read_csv('titani_data.csv')
-st.write(df)
+# st.write(df)
+
+# print(df.info())
+df['Embarked'] = df['Embarked'].fillna('Unknown')
+
+# get unique values for embarked port
+embarked_port = list(df['Embarked'].unique())
+gender = list(df['Sex'].unique())
+
+col1, col2 = st.columns([1,1])
+selected_port = col1.selectbox(options=embarked_port,
+                               label='Select a port')
+selected_gender = col2.selectbox(options=gender,
+                                 label='Select a gender')
+
